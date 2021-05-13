68eb57cf16edcc5fe54bbb49569b45fe979a04d0
Kavery pm <shiny.kavery@gmail.com>
2021-05-12 23:50:16 -0400

    first commit

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.css';
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>Date</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">300</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
