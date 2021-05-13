5c5b5d6c0ff71445141473c63706c26bd751ec90
Kavery pm <shiny.kavery@gmail.com>
2021-05-13 16:07:31 -0400

    added a date component and css styling

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,9 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 function ExpenseItem(props) {
-  const expenseDate= new Date(2021,2, 28);
+  
   return (
     <div className="expense-item">
-      <div>{props.date}</div>
+     <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
