6dc172570e6591c7f8939e97037a0760c9a33991
Kavery pm <shiny.kavery@gmail.com>
2021-05-13 15:25:03 -0400

    first comit

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,8 +1,9 @@
 import './ExpenseItem.css';
 function ExpenseItem() {
+  const expenseDate= new Date(2021,2, 28);
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__description">
         <h2>Car Insurance</h2>
         <div className="expense-item__price">300</div>
